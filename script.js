6a96aad877627cf0cd7b6c2a0d839ebd7d935d8d
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-02 19:35:57 -0500

    working on pseudocode

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log(`test`);
+//create new repository and clone from GitHub that contains an HTML and JS file
+//connect the script.js to index.html
+// console.log(`test`);
+//Create a platform using JS for freelance workers to advertise services
+//using DOM elements ensure "document.querySelector is used"
+//create arrays for the names and occupations
+
+//initialize an array of possible names and occupations
+const freelancers = [
+  { name: "Dr. Slice", price: 25, occupation: "gardener" },
+  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+  { name: "Prof. Prism", price: 81, occupation: "teacher" },
+  { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+  { name: "Prof. Spark", price: 76, occupation: "programmer" },
+  { name: "Dr. Wire", price: 47, occupation: "teacher" },
+  { name: "Prof. Goose", price: 72, occupation: "driver" },
+];
+
+//need to create a function that determines the average price of freelanceers
+
+//Program initializes an array of freelancers with names, occupations, and starting prices
+//initial array of freelancers is rendered to the HTML page from JS
